f04c8bade590192b25047f31b1c207c97f4ff271
satupa <satupalv@gmail.com>
2025-03-28 15:46:44 +0200

    first commits

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import sitemap from "@astrojs/sitemap";
 
 import cloudflare from "@astrojs/cloudflare";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), react()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
